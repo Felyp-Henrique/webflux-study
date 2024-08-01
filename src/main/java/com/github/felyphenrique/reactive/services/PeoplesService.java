33d409891cffc3dd6126286576e28b453b3ac9c2
Felyp-Henrique <felyphenrique2015@gmail.com>
2024-07-31 21:25:23 -0300

    init

---
+++ src/main/java/com/github/felyphenrique/reactive/services/PeoplesService.java
@@ -0,0 +1,23 @@
+package com.github.felyphenrique.reactive.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.felyphenrique.reactive.models.People;
+import com.github.felyphenrique.reactive.repositories.PeoplesRepository;
+
+import reactor.core.publisher.Flux;
+
+@Service()
+public class PeoplesService {
+    private final PeoplesRepository repository;
+
+    @Autowired()
+    public PeoplesService(PeoplesRepository repository) {
+        this.repository = repository;
+    }
+
+    public Flux<People> all() {
+        return Flux.empty();
+    }
+}
