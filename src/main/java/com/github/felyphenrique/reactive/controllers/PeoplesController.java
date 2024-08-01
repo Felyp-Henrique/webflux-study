8fceb8e83dc39c6fea40d67c50912e54a2075e84
Felyp-Henrique <felyphenrique2015@gmail.com>
2024-07-31 21:22:07 -0300

    init

--- src/main/java/com/github/felyphenrique/reactive/controllers/PeoplesController.java
+++ src/main/java/com/github/felyphenrique/reactive/controllers/PeoplesController.java
@@ -1,10 +1,20 @@
 package com.github.felyphenrique.reactive.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.felyphenrique.reactive.models.People;
+
+import reactor.core.publisher.Flux;
 
 @RestController()
 @RequestMapping("/peoples")
 public class PeoplesController {
 
+    @GetMapping()
+    public @ResponseBody() Flux<People> index() {
+        return Flux.empty();
+    }
 }
